643d75209b472bb6fdc077fe828b44d57344d613
Dhruva Kashyap <dhruvakashyap73@gmail.com>
2025-12-05 12:10:48 +0530

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Use your Supabase project URL and anon/public key
+// Use your Supabase:(PostgreSQL Dev Platform) project URL and anon/public key
 const supabaseUrl = 'https://lpcfgukzgmwcwezllslv.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwY2ZndWt6Z213Y3dlemxsc2x2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MTYyMDEsImV4cCI6MjA3NTQ5MjIwMX0.RUUMYIjCWK9PgsZxShBHYzW8YejhU6ueMYMFCyrgvKI' // Replace with your anon/public key
 
